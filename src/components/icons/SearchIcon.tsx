5eb11a8bc9bd94ce428afc7c976742527e1397e7
shakilshegu <muhammadshakil1968@gamil.com>
2025-08-18 11:50:19 +0530

    updated production errors

--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const Component = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 19.5 19.5" {...props}><path xmlns="http://www.w3.org/2000/svg" stroke="#4B5563" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M9.75.75v1m0 16v1m9-9h-1m-16 0h-1m15.36 6.364-.7-.707M4.09 4.093l-.7-.707m12.72 0-.7.707M4.09 15.407l-.7.707M13.75 9.75a4 4 0 1 1-7.999.001 4 4 0 0 1 7.999-.001" /></svg>;
 export default Component;
